bf1399ebd56c8418a003a63aba37d24ebfbca2cb
Brenon Araujo <brenon.araujo@devdojo.solutions>
2019-03-18 23:13:40 -0300

    The bomb has been planted!

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API_URL = "https://br1.api.riotgames.com/lol";
-const AUTH_TOKEN = "RGAPI-61b2fad0-18f7-4e1f-83c7-69bb2b2ef3ea";
+const AUTH_TOKEN = "RRGAPI-61b2fad0-18f7-4e1f-83c7-69bb2b2ef3ea";
 
 module.exports = {
   API_URL,
--- utils/constants.js
+++
@@ -1,7 +0,0 @@
-const API_URL = "https://br1.api.riotgames.com/lol";
-const AUTH_TOKEN = "RGAPI-5902f3df-8220-46ce-a6d8-590dee37a785";
-
-module.exports = { 
-    API_URL,
-    AUTH_TOKEN
-};
