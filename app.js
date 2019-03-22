4cda9d11f0148287bfe0b8c8db9b18dfcf38e20b
brenonaraujo <brenonaraujo@gmail.com>
2019-03-22 19:30:57 -0300

    mongodb connection added

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require('mongoose');
 const errorHandler = require("./src/middleware/error-handller");
 
+// API route files
 const SummonerApi = require("./src/api/summoner");
 
-// view engine setup
+const app = express();
+
+mongoose.connect(
+  config.database,
+  {
+    useMongoClient: true
+  },
+  error => {
+    if (error) console.log(error);
+  }
+);
+
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+// Middleware and configs
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Endpoints declaration
 app.use("/api/summoner", SummonerApi);
 
+// General Error handler
 app.use(errorHandler);
 
 module.exports = app;
